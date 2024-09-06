17562b4165a43985671dfa53e838db65bf94e628
matiaszo <tiaszuniga@gmail.com>
2024-09-06 16:39:13 -0300

    mapa finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import { ApiCard, PlayerCard } from './components/Card'
 import produtos from './constants/produtos.json'
 import { api } from "./api/rmApi"
 import style from './App.module.css'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import "leaflet-defaulticon-compatibility";
+
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Marker } from 'react-leaflet'
+import { Popup } from 'react-leaflet'
+
+
 
 function App() {
-  const [show, setShow] = useState("")
+  const [show, setShow] = useState("")  
   const [data, setData] = useState([])
   const [page, setPage] = useState("")
   const [name, setName] = useState("")
@@ -75,9 +85,19 @@ function App() {
       }
      {show === "map" &&
         <>
-      <h2>Mapa</h2>
-          <div>
-              mapa aqui
+      <h2>Mapa</h2>  
+          <div style={{display:'flex', justifyContent: 'center'}}>
+          <MapContainer style={{width:'90%', height: '700px'}} center={[-25.4249548,-49.2725292]} zoom={16} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-25.4249548,-49.2725292]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
           </div>
          </>
       }
